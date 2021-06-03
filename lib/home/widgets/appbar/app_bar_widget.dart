b482d1a9f0c87c5d96098a0bb268642ee903f19a
S4nches <luis-lipe@hotmail.com>
2021-06-03 11:58:37 -0300

    AppBar configured

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,44 @@
+import 'package:dev_quiz/core/app_gradients.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          child: Row(children: [
+                        Text("Ola, ", style: AppTextStyles.title),
+                        Text("Luis", style: AppTextStyles.titleBold),
+                      ])),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://avatars.githubusercontent.com/u/49485457?s=400&u=99e153af94934064dfaf290b6db85401362a6fde&v=4"))),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+              ],
+            ),
+          ),
+        );
+}
---
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -0,0 +1,27 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LevelButtonWidget extends StatelessWidget {
+  const LevelButtonWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.levelButtonFacil,
+        border: Border.fromBorderSide(
+            BorderSide(color: AppColors.levelButtonBorderFacil)),
+        borderRadius: BorderRadius.circular(28),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26.0, vertical: 6),
+        child: Text(
+          "Fácil",
+          style: GoogleFonts.notoSans(color: AppColors.white, fontSize: 13),
+        ),
+      ),
+    );
+  }
+}
